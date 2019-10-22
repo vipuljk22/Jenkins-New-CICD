c994cb4f418740368e5bd5ee606ee9227fcad725
Vipul Jha <vipuljha47@gmail.com>
2019-10-22 20:57:23 +0530

    test commit

--- Git Upload test/eventLogging/eventLogging.js
+++ Git Upload test/eventLogging/eventLogging.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     let eventJson = JSON.stringify(event);
-    console.log("Start : Logging event log22");
+    console.log("Start : Logging event log23");
     console.log(eventJson);
     return {
         statusCode: 200,
