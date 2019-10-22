3c8d938e43ef8a165287b72f9f11f646edd71621
vipuljk22 <vipuljha47@gmail.com>
2019-10-22 15:21:17 +0530

    hohugyfyg

--- Git Upload test/concatenateString/concatenateString.js
+++ Git Upload test/concatenateString/concatenateString.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     let {firstName, lastName} = JSON.parse(event.body);
-    console.log("Vipul Changes");
+    console.log("Vipul Changes22");
     return {
         statusCode: 200,
         body: JSON.stringify({
