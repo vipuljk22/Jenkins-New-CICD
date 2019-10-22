fa2490ff252dfd3d902f642de2f01063edf4e3a0
vipuljk22 <vipuljha47@gmail.com>
2019-10-22 23:22:48 +0530

    sixth

--- Git Upload test/eventLogging/eventLogging.js
+++ Git Upload test/eventLogging/eventLogging.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     let eventJson = JSON.stringify(event);
-    console.log("Start : Logging event log23");
+    console.log("Start : Logging event log25");
     console.log(eventJson);
     return {
         statusCode: 200,
